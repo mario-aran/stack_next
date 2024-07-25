0bfd95dd7c64938291096fcecf29b787ea76e9a1
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-07-25 18:39:00 -0400

    style: minor change in mui-theme

--- apps/web/src/config/themes/mui-theme.tsx
+++ apps/web/src/config/themes/mui-theme.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Roboto } from 'next/font/google';
 import { PropsWithChildren } from 'react';
 
-const isDarkMode = true;
+const isDarkMode = false;
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
